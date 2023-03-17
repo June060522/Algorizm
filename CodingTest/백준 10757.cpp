0af0081f4be5be60de65d2d66967576db4042c15
준이 <98876187+June060522@users.noreply.github.com>
2023-03-17 21:59:07 +0900

    4problem

--- CodingTest/백준 10757.cpp
+++ CodingTest/백준 10757.cpp
@@ -4,22 +4,22 @@
 //
 //int main()
 //{
-//	unsigned long long a = 0;
-//	unsigned long long b = 0;
+//	string a;
+//	string b;
 //
-//	vector<int> v;
 //	cin >> a >> b;
 //
-//	for (int i = 0; a > 0; i++)
+//	vector<int> v;
+//
+//
+//	for (int i = 0; i < a.length(); i++)
 //	{
-//		v.push_back(a % 10);
-//		a /= 10;
+//		v.push_back(((int)a[a.length() - i]) + 1);
 //	}
 //
-//	for (int j = 0; b > 0; j++)
+//	for (int j = 0; j < b.length(); j++)
 //	{
-//		v[j] += b % 10;
-//		b /= 10;
+//		v[j] += ((int)b[b.length()- j] +1);
 //	}
 //
 //	for (int i = 0; i < v.size(); i++)
@@ -38,51 +38,9 @@
 //		}
 //	}
 //
-//	for (int i = v.size() - 1; i >= 0; i--)
-//	{
-//		cout << v[i];
-//	}
-//}#include <iostream>
-//#include <vector>
-//using namespace std;
 //
-//int main()
-//{
-//	unsigned long long a = 0;
-//	unsigned long long b = 0;
-//
-//	vector<int> v;
-//	cin >> a >> b;
-//
-//	for (int i = 0; a > 0; i++)
-//	{
-//		v.push_back(a % 10);
-//		a /= 10;
-//	}
-//
-//	for (int j = 0; b > 0; j++)
-//	{
-//		v[j] += b % 10;
-//		b /= 10;
-//	}
 //
 //	for (int i = 0; i < v.size(); i++)
-//	{
-//		if (v[i] >= 10)
-//		{
-//			if (i + 1 == v.size())
-//			{
-//				v.push_back(1);
-//			}
-//			else
-//			{
-//				v[i + 1]++;
-//			}
-//			v[i] -= 10;
-//		}
-//	}
-//
-//	for (int i = v.size() - 1; i >= 0; i--)
 //	{
 //		cout << v[i];
 //	}
--- CodingTest/백준 1174번.cpp
+++ CodingTest/백준 1174번.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-
-using namespace std;
-
-bool f(int a);
-int cnt = 0;
-int main()
-{
-	int iii = -1;
-	int num = 0;
-	cin >> num;
-	do
-	{
-		iii++;
-		if (f(iii))
-			cnt++;
-	} while (num != cnt);
-	cout << iii;
-}
-
-bool f(int a)
-{
-	int b[8] = { 0 };
-	int k = 0;
-	for (int i = 0; a > 0; i++)
-	{
-		b[i] = a % 10;
-		a /= 10;
-		k++;
-	}
-	while (k > 0)
-	{
-		if (b[k] < b[k - 1])
-			return false;
-		k--;
-	}
-	return true;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//bool f(int a);
+//int cnt = 0;
+//int main()
+//{
+//	int iii = -1;
+//	int num = 0;
+//	cin >> num;
+//	do
+//	{
+//		iii++;
+//		if (f(iii))
+//			cnt++;
+//	} while (num != cnt);
+//	cout << iii;
+//}
+//
+//bool f(int a)
+//{
+//	int b[8] = { 0 };
+//	int k = 0;
+//	for (int i = 0; a > 0; i++)
+//	{
+//		b[i] = a % 10;
+//		a /= 10;
+//		k++;
+//	}
+//	while (k > 0)
+//	{
+//		if (b[k] < b[k - 1])
+//			return false;
+//		k--;
+//	}
+//	return true;
+//}
