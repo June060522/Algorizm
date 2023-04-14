22db8004f6045cfac38347181ea33c709b40252e
준이 <98876187+June060522@users.noreply.github.com>
2023-04-14 20:41:54 +0900

    2

---
+++ CodingTest/백준 15649.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string arrS[100000];
+bool check(string a, string b)
+{
+	string sa = "";
+	sa += a[0];
+	sa += a[1];
+	sa += a[2];
+	string sb = "";
+	sb += b[0];
+	sb += b[1];
+	sb += b[2];
+	int ai = stoi(sa);
+	int bi = stoi(sb);
+
+	if(ai < bi)
+		return true;
+
+	return false;
+}
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int repeat = 0;
+	cin >> repeat;
+	string s;
+	getline(cin, s);
+	for (int i = 0; i < repeat; i++)
+		getline(cin, arrS[i]);
+
+	sort(arrS, arrS + repeat, check);
+
+	for (int i = 0; i < repeat; i++)
+	{
+		cout << arrS[i] << "\n";
+	}
+}
--- CodingTest/백준 2504.cpp
+++ CodingTest/백준 2504.cpp
@@ -1,94 +1,94 @@
-#include <iostream>
-#include <stack>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-	stack<string> s;
-	char input = ' ';
-	int num = 0;
-	while (cin >> input)
-	{
-		if (input == 'a')
-			break;
-
-		if (input == ')')
-		{
-			if (s.top() != "72")
-			{
-				if (s.top() == "91" || s.top() == "93" || s.top() == "41")
-				{
-					cout << 0;
-					return 0;
-				}
-				else
-				{
-					s.pop();
-					if (s.top() == "93" || s.top() == "91" || s.top() == "40" || s.top() == "41")
-
-					s.push("2");
-				}
-			}
-			else
-			{
-				num = stoi(s.top());
-				s.pop();
-				if (s.top() == "93" || s.top() == "91" || s.top() == "40" || s.top() == "41")
-				{
-					cout << num * 2 << endl;
-					s.push(to_string(num * 2));
-				}
-				else
-				{
-					num *= 2;
-					num += stoi(s.top());
-					cout << num << endl;
-					s.pop();
-					s.push(to_string(num));
-				}
-			}
-		}
-		else if (input == ']')
-		{
-			if (s.top() != "91")
-			{
-				if (s.top() == "93" || s.top() == "40" || s.top() == "41")
-				{
-					cout << 0;
-					return 0;
-				}
-				else
-				{
-					num = stoi(s.top());
-					s.pop();
-					if (s.top() == "93" || s.top() == "91" || s.top() == "40" || s.top() == "41")
-					{
-						cout << num * 3 << endl;
-						s.push(to_string(num * 3));
-					}
-					else
-					{
-						num *= 3;
-						num += stoi(s.top());
-						s.pop();
-						cout << num << endl;
-						s.push(to_string(num));
-					}
-				}
-			}
-			else
-			{
-				s.pop();
-				s.push("3");
-			}
-		}
-		else
-		{
-			s.push(to_string(input));
-		}
-	}
-	cout << s.top() << endl;
-
-}
+//#include <iostream>
+//#include <stack>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	stack<string> s;
+//	char input = ' ';
+//	int num = 0;
+//	while (cin >> input)
+//	{
+//		if (input == 'a')
+//			break;
+//
+//		if (input == ')')
+//		{
+//			if (s.top() != "72")
+//			{
+//				if (s.top() == "91" || s.top() == "93" || s.top() == "41")
+//				{
+//					cout << 0;
+//					return 0;
+//				}
+//				else
+//				{
+//					s.pop();
+//					if (s.top() == "93" || s.top() == "91" || s.top() == "40" || s.top() == "41")
+//
+//					s.push("2");
+//				}
+//			}
+//			else
+//			{
+//				num = stoi(s.top());
+//				s.pop();
+//				if (s.top() == "93" || s.top() == "91" || s.top() == "40" || s.top() == "41")
+//				{
+//					cout << num * 2 << endl;
+//					s.push(to_string(num * 2));
+//				}
+//				else
+//				{
+//					num *= 2;
+//					num += stoi(s.top());
+//					cout << num << endl;
+//					s.pop();
+//					s.push(to_string(num));
+//				}
+//			}
+//		}
+//		else if (input == ']')
+//		{
+//			if (s.top() != "91")
+//			{
+//				if (s.top() == "93" || s.top() == "40" || s.top() == "41")
+//				{
+//					cout << 0;
+//					return 0;
+//				}
+//				else
+//				{
+//					num = stoi(s.top());
+//					s.pop();
+//					if (s.top() == "93" || s.top() == "91" || s.top() == "40" || s.top() == "41")
+//					{
+//						cout << num * 3 << endl;
+//						s.push(to_string(num * 3));
+//					}
+//					else
+//					{
+//						num *= 3;
+//						num += stoi(s.top());
+//						s.pop();
+//						cout << num << endl;
+//						s.push(to_string(num));
+//					}
+//				}
+//			}
+//			else
+//			{
+//				s.pop();
+//				s.push("3");
+//			}
+//		}
+//		else
+//		{
+//			s.push(to_string(input));
+//		}
+//	}
+//	cout << s.top() << endl;
+//
+//}
