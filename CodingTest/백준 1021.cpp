0e6a6bfacbf6fe6ce9d1803e17d163d2861884e1
준이 <98876187+June060522@users.noreply.github.com>
2023-04-27 23:17:17 +0900

    commit

--- CodingTest/백준 1021.cpp
+++ CodingTest/백준 1021.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-#include <deque>
-
-using namespace std;
-
-int main()
-{
-	deque<int> dq;
-	int size = 0;
-	int repeat = 0;
-	int answer = 0;
-	int input = 0;
-	cin >> size >> repeat;
-	for (int i = 1; i <= size; i++)
-		dq.push_back(i);
-
-	for (int i = 0; i < repeat; i++)
-	{
-		cin >> input;
-		
-	}
-}
+//#include <iostream>
+//#include <deque>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	deque<int> dq;
+//	int size = 0;
+//	int repeat = 0;
+//	int answer = 0;
+//	int input = 0;
+//	cin >> size >> repeat;
+//	for (int i = 1; i <= size; i++)
+//		dq.push_back(i);
+//
+//	for (int i = 0; i < repeat; i++)
+//	{
+//		cin >> input;
+//		
+//	}
+//}
