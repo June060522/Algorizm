a659cfbdcb14fdaeb01c718bba6c556d4b556c1a
준이 <98876187+June060522@users.noreply.github.com>
2023-05-06 23:49:43 +0900

    a

--- CodingTest/백준 2775.cpp
+++ CodingTest/백준 2775.cpp
@@ -1,21 +1,19 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	int DP[15][15];
-//	for (int i = 0; i < 15; i++)
-//	{
-//		DP[0][i] = i + 1;
-//		DP[i][0] = i + 1;
-//	}
-//
-//	for (int i = 1; i < 15; i++)
-//	{
-//		for (int j = 2; j < 15; j++)
-//		{
-//			DP[i][j] = DP[i - 1][j] + DP[i][j - 1];
-//		}
-//	}
-//}
+#include <iostream>
+using namespace std;
+
+int getNum(int a, int b) {
+    if (b == 1)
+        return 1;
+    if (a == 0)
+        return b;
+    return (getNum(a - 1, b) + getNum(a, b - 1));
+}
+
+int main() {
+    int T, k, n;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> k >> n;
+        cout << getNum(k, n) << '\n';
+    }
+}
