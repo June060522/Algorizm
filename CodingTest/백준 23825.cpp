4ae509307d9e9673e6fdfc2ee192ad639fc2d4bf
준이 <98876187+June060522@users.noreply.github.com>
2023-05-09 16:30:42 +0900

    1

--- CodingTest/백준 23825.cpp
+++ CodingTest/백준 23825.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
-#include <cmath>
+#include <queue>
 using namespace std;
 
 int main()
 {
-	int a, b;
-	cin >> a >> b;
-	a /= 2;
-	b /= 2;
-	cout << min(a, b);
+    int n, m, l;
+    int num;
+    int max = 0;
+    int save, plus = 0;
+    int saveL = 0;
+    int result = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        queue<int> q;
+        cin >> m >> l;
+        saveL = l;
+        for (int j = 0; j < m; j++)
+        {
+            cin >> num;
+            if (num > max)
+            {
+                max = num;
+            }
+            q.push(num);
+        }
+        while (q.front() != max)
+        {
+            save = q.front();
+            q.pop();
+            cout << q.front();
+            q.push(save);
+            if (saveL < 0)
+            {
+                plus++;
+                result = m - plus;
+            }
+            else
+            {
+                saveL--;
+                result = saveL;
+            }
+        }
+        cout << result << '\n';
+    }
 }
