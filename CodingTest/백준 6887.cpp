f5f3f96770a3ded6bcd1c2075f2f81e64a95c9b9
준이 <98876187+June060522@users.noreply.github.com>
2023-05-25 23:33:59 +0900

    1

---
+++ CodingTest/백준 6887.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	cin >> a;
+	a = sqrt(a);
+	cout << "The largest square has side length " << a << ".";
+}
