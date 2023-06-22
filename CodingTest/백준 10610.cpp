889b54507c533b40c44d94516c63c5818761b8a5
준이 <98876187+June060522@users.noreply.github.com>
2023-06-22 21:40:17 +0900

    1

---
+++ CodingTest/백준 10610.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int arr[100001];
+int main()
+{
+	string str;
+	int num = 0;
+	cin >> str;
+	for (int i = 0; i < str.length(); i++)
+	{
+		arr[i] = str[i] - '0';
+		num += arr[i];
+	}
+
+	sort(arr, arr + str.length(), greater<>());
+
+	if (num % 3 != 0 || arr[str.length() - 1] != 0)
+	{
+		cout << -1;
+		return 0;
+	}
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		cout << arr[i];
+	}
+}
