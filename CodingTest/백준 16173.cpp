d36255ff82baf62519552c46a3377e20a4d05ef1
준이 <98876187+June060522@users.noreply.github.com>
2023-07-17 10:58:57 +0900

    3

---
+++ CodingTest/백준 16173.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+bool visited[3][3];
+int zone[3][3];
+bool iswin = false;
+int zoneSize = 0;
+
+void Dfs(int x, int y)
+{
+	if (x >= zoneSize || y >= zoneSize || x < 0 || y < 0 || visited[x][y])
+		return;
+
+	visited[x][y] = true;
+
+	if (zone[x][y] == -1)
+	{
+		iswin = true;
+		return;
+	}
+
+	Dfs(x, y + zone[x][y]);
+	Dfs(x + zone[x][y], y);
+	Dfs(x, y - zone[x][y]);
+	Dfs(x - zone[x][y], y);
+}
+
+int main()
+{
+	cin >> zoneSize;
+	for (int i = 0; i < zoneSize; i++)
+	{
+		for (int j = 0; j < zoneSize; j++)
+		{
+			cin >> zone[i][j];
+		}
+	}
+
+	Dfs(0, 0);
+
+	if (!iswin)
+		cout << "Hing";
+	else
+		cout << "HaruHaru";
+}
--- CodingTest/백준 26549.cpp
+++ CodingTest/백준 26549.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-bool visited[9];
-vector<int> graph[9];
+bool visited[101][101];
+char zone[101][101];
+int dotCnt = 0;
+int zoneCnt = 0;
 
-void Bfs(int start) {
-	queue<int> q;
-	q.push(start);
-	visited[start] = true;
+void Dfs(int x, int y)
+{
+	if (visited[x][y] || zone[x][y] != '.')
+		return;
 
-	while (!q.empty()) {
-		int x = q.front();
-		q.pop();
-		cout << x << ' ';
-		for (int i = 0; i < graph[x].size(); i++) {
-			int y = graph[x][i];
-			if (!visited[y]) {
-				q.push(y);
-				visited[y] = true;
-			}
-		}
-	}
+	dotCnt++;
+	visited[x][y] = true;
+
+	Dfs(x + 1, y);
+	Dfs(x, y + 1);
 }
 
 int main()
@@ -31,15 +25,47 @@ int main()
 	int repeat = 0;
 	cin >> repeat;
 
-	for (int i = 0; i < repeat; i++)
+	for (int k = 0; k < repeat; k++)
 	{
-		int dotCnt = 0;
-		int zoneCnt = 0;
-		int x, y;
-		cin >> x >> y;
-		
-		char **zone = new char*[x];
-		for (int j = 0; j < x; j++)
-			zone[j] = new char[y];
+		zoneCnt = 0;
+		dotCnt = 0;
+
+		fill(&visited[0][0], &visited[100][100], false);
+		fill(&zone[0][0], &zone[100][100], ' ');
+
+		int n, m;
+		cin >> n >> m;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				cin >> zone[i][j];
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (zone[i][j] == '.' && !visited[i][j])
+				{
+					zoneCnt++;
+					dotCnt++;
+					visited[i][j] = true;
+					Dfs(i + 1, j);
+					Dfs(i, j + 1);
+				}
+			}
+		}
+
+		if (zoneCnt > 1)
+			cout << zoneCnt << " sections, ";
+		else
+			cout << zoneCnt << " section, ";
+
+		if (dotCnt > 1)
+			cout << dotCnt << " spaces\n";
+		else
+			cout << dotCnt << " space\n";
 	}
 }
