76da478d7387c858c361a51a4f9f2a14497fa87e
준이 <98876187+June060522@users.noreply.github.com>
2023-08-01 16:44:24 +0900

    3

---
+++ CodingTest/백준 12851.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+bool visited[300001];
+vector<int> pos[300001];
+int x, y, answer = 0;
+int cnt = 0;
+
+void Bfs(int start)
+{
+	queue<int> q;
+	q.push(start);
+	visited[start] = true;
+
+	while (!q.empty())
+	{
+		answer++;
+		vector<int> saveVec;
+
+		while (!q.empty())
+		{
+			saveVec.push_back(q.front());
+			q.pop();
+		}
+		for (int j = 0; j < saveVec.size(); j++)
+		{
+			for (int i = 0; i < pos[saveVec[j]].size(); i++)
+			{
+				int z = pos[saveVec[j]][i];
+				if (z == y)
+					cnt++;
+				if (!visited[z])
+				{
+					q.push(z);
+					visited[z] = true;
+				}
+			}
+		}
+
+		if (cnt != 0)
+			return;
+	}
+}
+
+int main()
+{
+	for (int i = 0; i <= 150000; i++)
+	{
+		pos[i].push_back(i * 2);
+		pos[i].push_back(i + 1);
+
+		if (i != 0)
+			pos[i].push_back(i - 1);
+	}
+
+	cin >> x >> y;
+	if (x == y)
+		cout << 0 << '\n' << 1;
+	else
+	{
+		Bfs(x);
+		cout << answer << '\n' << cnt;
+	}
+}
--- CodingTest/백준 3017.cpp
+++ CodingTest/백준 3017.cpp
@@ -1,10 +1,92 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	string input1, input2;
-//
-//	s
-//}
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool visited[61];
+char input2Arr[61];
+bool complete = false;
+string input1, input2, answer;
+void BackTrackingUp(string& a, int idx)
+{
+	if (complete || a < input1)
+		return;
+	if (idx >= input1.length())
+	{
+		complete = true;
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < input2.length(); ++i)
+		{
+			if (visited[i] || (idx == 0 && input2Arr[i] == '0'))continue;
+			visited[i] = true;
+			a[idx] = input2Arr[i];
+			BackTrackingUp(a, idx + 1);
+			if (complete)
+				return;
+			visited[i] = false;
+		}
+	}
+}
+
+void BackTrackingDown(string& a, int idx)
+{
+	if (complete || a >= input1)
+		return;
+
+	if (idx >= input1.length())
+	{
+		complete = true;
+		return;
+	}
+	else
+	{
+		for (int i = input2.length() - 1; i >= 0; --i)
+		{
+			if (visited[i] || (idx == 0 && input2Arr[i] == '0'))continue;
+			visited[i] = true;
+			a[idx] = input2Arr[i];
+			BackTrackingDown(a, idx + 1);
+			if (complete)
+				return;
+			visited[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	cin >> input1 >> input2;
+
+	for (int i = 0; i < input2.length(); i++)
+	{
+		input2Arr[i] = input2[i];
+	}
+
+	sort(input2Arr, input2Arr + input2.length());
+
+	for (int i = 0; i < input1.length(); i++)
+		answer += '9';
+	BackTrackingUp(answer, 0);
+	if (!complete)
+		answer = "0";
+	complete = false;
+	cout << answer << '\n';
+	answer = "";
+
+	fill(visited, visited + 61, false);
+
+	for (int i = 0; i < input1.length(); i++)
+		answer += '0';
+	BackTrackingDown(answer, 0);
+	if (!complete)
+		answer = "0";
+	cout << answer;
+}
