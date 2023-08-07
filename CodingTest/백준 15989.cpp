dbd2c699f3d5093936d52a42d8cdc2ec2f45b3e9
준이 <98876187+June060522@users.noreply.github.com>
2023-08-07 17:14:33 +0900

    3

---
+++ CodingTest/백준 15989.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//
+//using namespace std;
+//long long int Dp[10001] = {0,1,2,3,4, 5};
+//
+//int main()
+//{
+//	for (int i = 4; i < 10001; i++)
+//	{
+//		Dp[i] = 0;
+//	}
+//
+//	int repeat,n;
+//	cin >> repeat;
+//
+//	for (int i = 0; i < repeat; i++)
+//	{
+//		cin >> n;
+//		cout << Dp[n] << '\n';
+//	}
+//}
