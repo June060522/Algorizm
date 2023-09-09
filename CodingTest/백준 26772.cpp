cf1bb55a0ed0986615920de6fb012d0f4e556086
준이 <98876187+June060522@users.noreply.github.com>
2023-09-09 11:28:50 +0900

    1

---
+++ CodingTest/백준 26772.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (size_t i = 0; i < n; i++)
+		cout << " @@@   @@@  ";
+		cout << '\n';
+	for (size_t i = 0; i < n; i++)
+		cout << "@   @ @   @ ";
+		cout << '\n';
+	for (size_t i = 0; i < n; i++)
+		cout << "@    @    @ ";
+		cout << '\n';
+	for (size_t i = 0; i < n; i++)
+		cout << "@         @ ";
+		cout << '\n';
+	for (size_t i = 0; i < n; i++)
+		cout << " @       @  ";
+		cout << '\n';
+	for (size_t i = 0; i < n; i++)
+		cout << "  @     @   ";
+		cout << '\n';
+	for (size_t i = 0; i < n; i++)
+		cout << "   @   @    ";
+		cout << '\n';
+	for (size_t i = 0; i < n; i++)
+		cout << "    @ @     ";
+		cout << '\n';
+	for (size_t i = 0; i < n; i++)
+		cout << "     @      ";
+}
