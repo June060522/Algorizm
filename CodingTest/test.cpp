0e70dcc2c472eaefc70745f7907fe6677f3f8aa9
준이 <98876187+June060522@users.noreply.github.com>
2023-11-05 23:44:53 +0900

    temp

--- CodingTest/test.cpp
+++ CodingTest/test.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
-bool test()
-{
-	bool a = false;
-	if (a)
-		return false;
-
-	if (rand() % 2 == 0)
-		a = true;
-	else
-		a = false;
-}
-
 int main()
 {
-	srand((unsigned int)time(NULL));
-	int a = 0, b = 0;
-	for (int i = 0; i < 10000000; i++)
-	{
-		if (test())
-			a++;
-		else
-			b++;
-	}
-	cout << a << endl << b;
+	queue<int> q;
+	q.pop();
 }
