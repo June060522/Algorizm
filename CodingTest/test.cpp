366ed816ec89fc28c4f0d876e637dc8ae2658b83
준이 <98876187+June060522@users.noreply.github.com>
2023-11-09 15:33:53 +0900

    test

--- CodingTest/test.cpp
+++ CodingTest/test.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-#include <queue>
+#include <deque>
 
 using namespace std;
 
 int main()
 {
-	queue<int> q;
-	q.pop();
+	int x;
+	cin >> x;
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			//수행
+		}
+	}
+
+	for (int i = 0; i < x; i++)
+	{
+		// 수행
+	}
 }
